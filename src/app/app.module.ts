d05a5fc27f9053f83cee48fb713ec2b672dc43a8
Izabella Loyse Cândido <izabellaloyse13@gmail.com>
2023-09-06 17:34:42 +0000

    feat(home): created installPWA method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { LayoutComponent } from './modules/layout/components/layout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LayoutComponent,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/home/components/home.component.ts
+++ src/app/modules/home/components/home.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public isInstalled = false;
 
+  constructor(private platform: Platform) {}
+
+  ngOnInit(): void {
+    window.addEventListener('beforeinstallprompt', (event: Event) => {
+      event.preventDefault();
+    });
+  }
+
+  public isCanInstallPWA(): boolean {
+    return (
+      this.platform.isBrowser &&
+      'serviceWorker' in navigator &&
+      'beforeinstallprompt' in window
+    );
+  }
+
+  public handleInstallPWA(): void {
+    const promptEvent = (window as any).beforeinstallprompt;
+
+    if (promptEvent) {
+      promptEvent.prompt();
+
+      promptEvent.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          this.isInstalled = true;
+        }
+      });
+    }
+  }
 }
